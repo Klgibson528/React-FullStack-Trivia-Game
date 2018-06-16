353b3b221d976f686fa83cf2570b32d9da57e873
Katy Gibson <klgibson528@gmail.com>
2018-06-16 13:21:59 -0500

    added comments and fixed auto-escaping characters

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const axios = require("axios");
 const cors = require("cors");
 
 var app = express();
-
+//cors is inserted to allow the two servers(front-end and back-end) to run together, may want to add extra security for deployment
+//adds header to let any url access your API
+//to make more specific, add react app url instead of star to say only that url can use API
 app.use(cors());
 app.use(express.static("build"));
 
@@ -12,7 +14,10 @@ app.get("/api/get-questions", function(req, res, next) {
   axios
     .get("https://opentdb.com/api.php?amount=10")
     .then(function(response) {
-      console.log(response.data); // Print the google web page.
+      //.data is built into axios which returns JSON from API call
+      console.log(response.data);
+      //.json is used because we are sending out JSON
+      //response.data.results is taking the response from the API call, calling the data method to get the data, and then targeting the results section of the JSON
       res.json({ questions: response.data.results });
     })
     .catch(next);
--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import React, { Component } from "react";
 import "./App.css";
+//make calls to API
 import axios from "axios";
 
 class App extends Component {
   constructor(props) {
     super(props);
+    //setting initial state
     this.state = { questions: [] };
+    //basically binding to this class so you can use it later
     this.get_questions();
   }
-
+  //we keep this method in the class becuase it deals with state
   get_questions(event) {
+    //calling api we created on back-end
     axios
       .get("http://localhost:9090/api/get-questions")
       .then(response => {
         console.log(response.data);
+        //setting state with new set of questions
         this.setState({ questions: response.data.questions });
+        //'questions' is the key, 'response.data.questions is the value
+        //this.state is now all the data in questions
       })
       .catch(error => {});
   }
@@ -23,10 +30,17 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Hello2</h1>
+        {/* when using an on- event the below format is recommended */}
+        {/* on click event the get_questions function will run and return a new set of questions */}
         <button onClick={e => this.get_questions(e)}>Get questions</button>
         <ul>
+          {/* using map to loop through the questions in the list and return an li */}
+          {/* this.state.questions is defined above in setState */}
           {this.state.questions.map(q => {
-            return <li>{q.question}</li>;
+            {
+              /* way to stop react from automatically escaping special characters */
+            }
+            return <li dangerouslySetInnerHTML={{ __html: q.question }} />;
           })}
         </ul>
       </div>
